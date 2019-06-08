5ac62cc16e8b5b88cfb712b57dc9d5daff7ab5b4
Jordan Chernof <jordan.chernof@gmail.com>
2019-06-08 15:20:02 -0400

    update host/port label in console

--- server.js
+++ server.js
@@ -706,5 +706,5 @@ var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening at http://127.0.0.1:%s", port)
 })
